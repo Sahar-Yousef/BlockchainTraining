dcb550c47016bfe57a48940def71052aab3c016b
Sallha Aladwani <sell.lla.78@gmail.com>
2022-07-18 14:11:21 +0300

    Create SoldityBasics.sol
    Practice Soldity basics.

---
+++ SoldityBasics.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.1;
+
+contract helloWorld {
+
+ function IsMultibale(uint n, uint m) public view returns (bool) {
+     bool x;
+     if (n % m == 0) {
+         x = true;
+     } else {
+         x = false;
+     }
+     return x;
+ }
+ 
+ function Max(uint n, uint m) public view returns (uint) {
+     uint y;
+     if (n > m) {
+         y = n;
+     } else {
+         y = m;
+     }
+     return y;
+ }
+}
